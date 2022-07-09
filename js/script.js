2f75d218bb1ef189ec04f01ab1e2ae3897d4e58f
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-07-09 08:14:54 +0200

    Refactoring cone using .map() and join()

--- js/script.js
+++ js/script.js
@@ -1,29 +1,42 @@
 const hero = {
   elementId: 'hero',
-  name: 'Player1',
+  name: 'Wizard',
   avatar: 'images/wizard.png',
   health: 60,
-  diceRoll: 6,
+  diceRoll: [3, 1, 4],
+  diceCount: 3,
 };
 
 const monster = {
   elementId: 'monster',
-  name: 'Player2',
+  name: 'Orc',
   avatar: 'images/orc.png',
   health: 10,
-  diceRoll: 4,
+  diceRoll: [2],
+  diceCount: 1,
 };
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, diceRoll } = data;
-  document.getElementById(elementId).innerHTML = `
-    <div class="character-card">
-    <h4 class="name"> ${name} </h4>
-    <img class="avatar" src="${avatar}"/>
-    <p class="health">health: <b> ${health} </b></p>
-    <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
-    </div> 
-`;
+  const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+
+  const diceHtml = diceRoll.map(function(getNum){
+    return `<div class="dice">${getNum}</div>`
+  }).join('')
+
+
+//   for (let i = 0; i < diceCount; i++) {
+//     diceHtml += `<div class="dice">${diceRoll[i]}</div>`;
+//   }
+
+  document.getElementById(elementId).innerHTML = `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">
+                ${diceHtml}
+            </div>
+        </div>`;
 }
+
 renderCharacter(hero);
 renderCharacter(monster);
