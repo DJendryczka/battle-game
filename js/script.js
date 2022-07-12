f4573b36d6eb8da38da784f5e59a20a4bbdfb532
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-07-12 06:27:20 +0200

    Refactoring constructor and adding render function

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ function getDiceRollArray(diceCount) {
   });
 }
 
-
 const hero = {
   elementId: 'hero',
   name: 'Wizard',
@@ -23,17 +22,17 @@ const monster = {
 
 function Character(data) {
   Object.assign(this, data);
-  this.getDiceHtml = function(diceCount) {
+  this.getDiceHtml = function (diceCount) {
     return getDiceRollArray(diceCount)
       .map(function (num) {
         return `<div class="dice">${num}</div>`;
       })
       .join('');
-  }
+  };
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceCount } = this;
     const diceHtml = this.getDiceHtml(diceCount);
-    document.getElementById(elementId).innerHTML = `<div class="character-card">
+    return `<div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}" />
             <div class="health">health: <b> ${health} </b></div>
@@ -45,58 +44,9 @@ function Character(data) {
 }
 const wizard = new Character(hero);
 const orc = new Character(monster);
-wizard.getCharacterHtml();
-orc.getCharacterHtml();
-
-// function renderCharacter(data) {
-//   const { elementId, name, avatar, health, diceCount } = data;
-
-//   const diceHtml = getDiceHtml(diceCount);
-
-//   document.getElementById(elementId).innerHTML = `<div class="character-card">
-//             <h4 class="name"> ${name} </h4>
-//             <img class="avatar" src="${avatar}" />
-//             <div class="health">health: <b> ${health} </b></div>
-//             <div class="dice-container">
-//                 ${diceHtml}
-//             </div>
-//         </div>`;
-// }
-
-// renderCharacter(hero);
-// renderCharacter(monster);
-
-// const hotel1 = {
-// 	name: 'Safari View',
-// 	rooms: 30,
-// 	stars: '⭐⭐⭐⭐⭐',
-// 	costPerNightAdult: 240,
-// 	costPerNightChild: 180,
-// }
-
-// const hotel2 = {
-// 	name: 'Leopard Mansion',
-// 	rooms: 96,
-// 	stars: '⭐⭐⭐',
-// 	costPerNightAdult: 120,
-// 	costPerNightChild: 180,
-// }
-
-// function NationalParkHotels(data){
-//     this.name = data.name
-//     this.rooms = data.rooms
-//     this.stars = data.stars
-//     this.costPerNightAdult = data.costPerNightAdult
-//     this.costPerNightChild = data.costPerNightChild
-
-// 	this.summariseHotel = function(){
-// 		const totalPrice = this.costPerNightAdult * 2 + this.costPerNightChild * 2
-// 		console.log(`A one night stay at the ${this.name} for two adults and
-// 		two children costs a total of ${totalPrice}`)
-// 	}
-// }
-
-// const safariView = new NationalParkHotels(hotel1)
-// const leopardMansion = new NationalParkHotels(hotel2)
-// safariView.summariseHotel()
-// leopardMansion.summariseHotel()
+function render() {
+  document.getElementById(wizard.elementId).innerHTML =
+    wizard.getCharacterHtml();
+  document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+}
+render();
