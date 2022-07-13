09a65f18a849716ed1dc1ba3feed9d08036b58f0
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-07-13 06:44:10 +0200

    Refactoring cone by nesting 2 obj. to one obj

--- js/script.js
+++ js/script.js
@@ -3,22 +3,24 @@ function getDiceRollArray(diceCount) {
     return Math.floor(Math.random() * 6 + 1);
   });
 }
+const characterData = {
+   hero : {
+    elementId: 'hero',
+    name: 'Wizard',
+    avatar: 'images/wizard.png',
+    health: 60,
+    diceCount: 3,
+  },
+  
+   monster : {
+    elementId: 'monster',
+    name: 'Orc',
+    avatar: 'images/orc.png',
+    health: 10,
+    diceCount: 1,
+  }
+}
 
-const hero = {
-  elementId: 'hero',
-  name: 'Wizard',
-  avatar: 'images/wizard.png',
-  health: 60,
-  diceCount: 3,
-};
-
-const monster = {
-  elementId: 'monster',
-  name: 'Orc',
-  avatar: 'images/orc.png',
-  health: 10,
-  diceCount: 1,
-};
 
 function Character(data) {
   Object.assign(this, data);
@@ -42,8 +44,8 @@ function Character(data) {
         </div>`;
   };
 }
-const wizard = new Character(hero);
-const orc = new Character(monster);
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
 function render() {
   document.getElementById(wizard.elementId).innerHTML =
     wizard.getCharacterHtml();
