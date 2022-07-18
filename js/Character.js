c88babf8a7c8b564e9090729b0cf04d4e9d09fea
Dariusz Jendryczka <dariusz.jendryczka@gmail.com>
2022-07-18 06:47:43 +0200

    Add healthbar to game

--- js/Character.js
+++ js/Character.js
@@ -1,12 +1,19 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js';
 
 const getPercentage = (remainingHealth, maximumHealth) =>
-    (100 * remainingHealth) / maximumHealth
+  (100 * remainingHealth) / maximumHealth;
 function Character(data) {
   Object.assign(this, data);
-
+  this.maxHealth = this.health;
   this.diceArray = getDicePlaceholderHtml(this.diceCount);
-  this.maxHealth = this.health
+
+  this.getDiceHtml = function () {
+    this.currentDiceScore = getDiceRollArray(this.diceCount);
+    this.diceArray = this.currentDiceScore
+      .map((num) => `<div class="dice">${num}</div>`)
+      .join('');
+  };
+  
   this.takeDamage = function (attackScoreArray) {
     const totalAttackScore = attackScoreArray.reduce(
       (total, currentScore) => total + currentScore
@@ -17,21 +24,26 @@ function Character(data) {
       this.health = 0;
     }
   };
-  this.getDiceHtml = function () {
-    this.currentDiceScore = getDiceRollArray(this.diceCount);
-    this.diceArray = this.currentDiceScore
-      .map((num) => `<div class="dice">${num}</div>`)
-      .join('');
+  this.getHealthBarHtml = function () {
+    const percent = getPercentage(this.health, this.maxHealth);
+    console.log(percent)
+    return `<div class="health-bar-outer">
+        <div class="health-bar-inner  ${percent < 26 ? "danger" : ""}" 
+        style="width: ${percent}%;">
+        </div>
+        </div>`;
   };
+ 
 
   this.getCharacterHtml = function () {
     const { elementId, name, avatar, health, diceCount } = this;
-
+    const healthBar = this.getHealthBarHtml();
     return `
           <div class="character-card">
               <h4 class="name"> ${name} </h4>
               <img class="avatar" src="${avatar}" />
               <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
               <div class="dice-container">
                   ${this.diceArray}
               </div>
